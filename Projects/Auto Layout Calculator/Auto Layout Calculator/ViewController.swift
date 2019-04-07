125d169655d2bb61ea57f3585df5ff4ab2a0b8c5
Gustavo <gustavo.goncalvespires@gmail.com>
2019-04-07 17:48:55 -0300

    Auto Layout Projects

---
+++ Projects/Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Gustavo Mac Mini on 07/04/19.
+//  Copyright © 2019 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
