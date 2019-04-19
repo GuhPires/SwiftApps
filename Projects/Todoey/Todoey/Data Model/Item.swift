9b5473b4bb5d924658e55b2262eda818d2b764e9
Gustavo <gustavo.goncalvespires@gmail.com>
2019-04-19 20:16:31 -0300

    Todoey App in ddevellopment

---
+++ Projects/Todoey/Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gustavo Mac Mini on 19/04/19.
+//  Copyright © 2019 DEVX. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
