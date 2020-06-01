ea86749491b2cd72478b1c56844d23e53d572c22
Gustavo <gustavo.goncalvespires@gmail.com>
2020-06-01 15:16:44 -0300

    Added GoalsPost App and its initial setup and UI

---
+++ Projects/GoalPostApp/GoalPostApp/Controller/GoalsVC.swift
@@ -0,0 +1,28 @@
+//
+//  GoalsVC.swift
+//  GoalPostApp
+//
+//  Created by Gustavo Mac Mini on 01/06/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func onCreatePressed(_ sender: Any) {
+        print("Creating...");
+    }
+    
+}
+
---
+++ Projects/GoalPostApp/GoalPostApp/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  GoalPostApp
+//
+//  Created by Gustavo Mac Mini on 01/06/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgress progress: Int) {
+        goalDescriptionLbl.text = description
+        goalTypeLbl.text = type
+        goalProgressLbl.text = "\(progress)"
+    }
+}
