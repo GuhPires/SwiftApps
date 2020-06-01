2daa2240bf7f20826103d3acbe795a8e5d1c7148
Gustavo <gustavo.goncalvespires@gmail.com>
2020-06-01 17:23:35 -0300

    GoalPostApp - Created View Controller to add new goal and its UI

---
+++ Projects/GoalPostApp/GoalPostApp/Controller/CreateGoalVC.swift
@@ -0,0 +1,36 @@
+//
+//  CreateGoalVC.swift
+//  GoalPostApp
+//
+//  Created by Gustavo Mac Mini on 01/06/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var goalTxtView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    @IBAction func onShortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onLongTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onNextBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onBackBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil);
+    }
+}
